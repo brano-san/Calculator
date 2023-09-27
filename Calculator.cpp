71c39dd69607ee2ffab4913216ef1c55d0203d60
brano-san <branoverv10@bk.ru>
2023-09-27 18:06:16 +0300

    Add the most part of Calculator
    Released binary and unary + and -; working parentheses
    Fixed parantheses work
    Added multiply and division
    Added all trigonometric functions and all operations
    Add templates and eval-function
    Add factorial, log, root and minor changes
    Fix code for functions with base
    Add exp-function and constants: E, PI

---
+++ Calculator.cpp
@@ -0,0 +1,308 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <map>
+
+namespace calculator
+{
+	class Math
+	{
+	public:
+		constexpr static double e  = 2.718281828459045;
+		constexpr static double pi = 3.141592653589793;
+
+		template<typename T>
+		static T factorial(T const& n)
+		{
+			int res = 1;
+			for (int i = 2; i <= n; ++i)
+				res *= i;
+			return res;
+		}
+
+		template<typename T>
+		static T log(T const& expression, double const& base)
+		{
+			return std::log(expression) / std::log(base);
+		}
+
+		template<typename T>
+		static T root(T const& expression, double const& power)
+		{
+			return std::pow(expression, static_cast<T>(-1) / power);
+		}
+	};
+
+	template<typename T = double>
+	class Calculator
+	{
+	public:
+		T eval(std::string const& expression)
+		{
+			if (expression.empty())
+				return 0;
+
+			_index = 0;
+			_expr = expression;
+			_expr.erase(std::remove_if(_expr.begin(), _expr.end(), isspace), _expr.end());
+
+			std::transform(_expr.begin(), _expr.end(), _expr.begin(),
+				[](unsigned char const& c) { return std::tolower(c); });
+			
+			return parseExpression();
+		}
+
+	private:
+		enum class OperationType
+		{
+			NULL_OP = 0,
+			ADDITION,       // +
+			SUBTRACTION,    // -
+			MULTIPLICATION, // *
+			DIVISION,		// /
+			EXTENT,			// ^
+			FACTORIAL		// !
+		};
+
+		class Operation
+		{
+		public:
+			Operation(OperationType const& type,
+				int const& priority,
+				bool const& isRightside = false)
+				: type(type), priority(priority), isRightside(isRightside)
+			{}
+
+			OperationType type;
+			int priority;
+			bool isRightside;
+		};
+
+		class OperationValue
+		{
+		public:
+			OperationValue(Operation const& op, T const& value)
+				: operation(op), value(value)
+			{}
+
+			bool isNullOperation() const
+			{
+				return (operation.type == OperationType::NULL_OP);
+			}
+
+			Operation operation;
+			T value;
+		};
+
+	private:
+		std::string _expr;
+		std::size_t _index{};
+		std::stack<OperationValue> _operations;
+
+	private:
+		T compute(T const& a, T const& b, Operation const& op) const
+		{
+			switch (op.type)
+			{
+			case OperationType::ADDITION:
+				return (a + b);
+			case OperationType::SUBTRACTION:
+				return (a - b);
+			case OperationType::MULTIPLICATION:
+				return (a * b);
+			case OperationType::DIVISION:
+				return (a / b);
+			case OperationType::EXTENT:
+				return std::pow(a, b);
+			case OperationType::FACTORIAL:
+				return Math::factorial(a);
+			case OperationType::NULL_OP:
+				return b;
+			}
+			throw std::runtime_error("Bad Operation (compute function)");
+		}
+
+		T compute(T const& expression, std::string const& operation, double const& base = 0) const
+		{
+			if (operation == "sin")
+				return std::sin(expression);
+			if (operation == "cos")
+				return std::cos(expression);
+			if (operation == "tg")
+				return (std::sin(expression) / std::cos(expression));
+			if (operation == "ctg")
+				return (std::cos(expression) / std::sin(expression));
+			if (operation == "sec")
+				return (1. / std::cos(expression));
+			if (operation == "csc")
+				return (1. / std::sin(expression));
+			if (operation == "ln")
+				return std::log(expression);
+			if (operation == "sqrt")
+				return std::sqrt(expression);
+			if (operation == "log")
+				return Math::log(expression, base);
+			if (operation == "root")
+				return Math::root(expression, base);
+			if (operation == "exp")
+				return std::exp(expression);
+			throw std::runtime_error("Bad Operation (trigonometric compute function)");
+		}
+
+		T parseExpression()
+		{
+			_operations.push(OperationValue(Operation(OperationType::NULL_OP, 0), 0));
+
+			auto value = parseToken();
+
+			while (!_operations.empty())
+			{
+				auto oper = parseOperator();
+
+				while (oper.priority <= _operations.top().operation.priority &&
+					oper.isRightside == false)
+				{
+					if(_operations.top().isNullOperation())
+					{
+						_operations.pop();
+						return value;
+					}
+
+					value = compute(_operations.top().value, value, _operations.top().operation);
+					_operations.pop();
+				}
+
+				_operations.push(OperationValue(oper, value));
+				if (!_operations.top().operation.isRightside)
+					value = parseToken();
+			}
+
+			return value;
+		}
+
+		Operation parseOperator()
+		{
+			if (_expr.size() == _index)
+				return Operation{ OperationType::NULL_OP, 0 };
+
+			switch (_expr[_index++])
+			{
+			case '+':
+				return Operation{ OperationType::ADDITION, 1 };
+			case '-':
+				return Operation{ OperationType::SUBTRACTION, 1 };
+			case '*':
+				return Operation{ OperationType::MULTIPLICATION, 2 };
+			case '/':
+				return Operation{ OperationType::DIVISION, 2 };
+			case '^':
+				return Operation{ OperationType::EXTENT, 3 };
+			case '!':
+				return Operation{ OperationType::FACTORIAL, 4, true };
+			default:
+				return Operation{ OperationType::NULL_OP, 0 };
+			}
+		}
+
+		T parseToken()
+		{
+			if (_expr.size() == _index)
+				return 0;
+
+			if (isdigit(_expr[_index]))
+			{
+				std::string	value;
+				while (isdigit(_expr[_index]) || _expr[_index] == '.')
+				{
+					value += _expr[_index++];
+				}
+				return std::stod(value);
+			}
+			if (_expr[_index] == '(') //expression in parentheses
+			{
+				++_index;
+				return parseExpression();
+			}
+			if (_expr[_index] == '|') //expression in module
+			{
+				++_index;
+				return std::abs(parseExpression());
+			}
+			if (_expr[_index] == '-') //unary minus
+			{
+				++_index;
+				return static_cast<T>(-1) * parseToken();
+			}
+			if (_expr[_index] == '+') //unary plus
+			{
+				++_index;
+				return parseToken();
+			}
+
+			const std::map<std::string, double> constants {
+				{ "e",  Math::e  },
+				{ "pi", Math::pi } };
+			for (auto const& constant : constants)
+			{
+				if (std::strncmp(&_expr[_index], constant.first.c_str(), constant.first.length()) == 0)
+				{
+					_index += constant.first.length();
+					return constant.second;
+				}
+			}
+
+			const std::string funcWithBase[]{ "log", "root" };
+			for (auto const& func : funcWithBase)
+			{
+				if (std::strncmp(&_expr[_index], func.c_str(), func.length()) == 0)
+				{
+					_index += func.length();
+					std::string base;
+					while (isdigit(_expr[_index]))
+					{
+						base += _expr[_index++];
+					}
+					if (base.empty())
+						base = (func == "log") ? "10" : "2";
+					++_index;
+					return compute(parseExpression(), func, std::stod(base));
+				}
+			}
+
+			const std::string functions[]{ "sin", "cos", "tg", "ctg", "sec", "csc", "ln", "sqrt", "exp" };
+			for (auto const& func : functions)
+			{
+				if (std::strncmp(&_expr[_index], func.c_str(), func.length()) == 0)
+				{
+					_index += func.length() + 1; // +1 for open bracket '('
+					return compute(parseExpression(), func);
+				}
+			}
+
+			throw std::runtime_error("Invalid token (parseToken function)");
+		}
+	};
+
+	template<typename T = double>
+	T eval(std::string const& expression)
+	{
+		Calculator<T> calc;
+		return calc.eval(expression);
+	}
+}
+
+int main()
+{
+	const std::string expression { "|e^pi| - (-1)" };
+
+	try
+	{
+		std::cout << std::fixed << calculator::eval<double>(expression) << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,167 @@
-# type_traits
-Implementation of some standart's type_traits
+<h1 align="center">
+  Calculator
+</h1>
+
+<p align="center">
+  An Awesome Calculator To Use It In Your Project
+  <br/>
+</p>
+
+## Table Of Contents
+* [About the Project](#about-the-project)
+* [Getting Started](#getting-started)
+  * [Installation](#installation)
+* [Usage](#usage)
+  * [Supported Operations And Functions](#supported-operations-and-functions)
+  * [Example](#example)
+* [License](#license)
+* [Authors](#authors)
+
+## About The Project
+
+Calculator is an expression parser that takes a string expression and returns an answer
+
+```C++
+double eval(std::string const& expression);
+```
+
+## Getting Started
+
+Here is a way how you can use the Calculator in your project
+
+### Installation
+
+1. Get ```Calculator.hpp``` on your project folder
+2. Connect ```Calculator.hpp``` to project file
+
+```C++
+#include "Calculator.hpp"
+```
+
+## Usage
+
+### Supported Operations And Functions
+
+<table>
+  <tr align="center">
+    <td><b> Operation/Function </b></td>
+    <td><b> Description </b></td>
+    <td><b> Example </b></td>
+    <td><b> Result </b></td>
+  </tr>
+  <tr align="center">
+    <td> + </td>
+    <td> Unary/Binary Plus Operation </td>
+    <td> (+10) <br/> 10 + 5 </td>
+    <td> 10 <br/> 15 </td>
+  </tr>
+  <tr align="center">
+    <td>-</td>
+    <td> Unary/Binary Minus Operation </td>
+    <td> (-10) <br/> 10 - 4.5 </td>
+    <td> -10 <br/> 5.5 </td>
+  </tr>
+  <tr align="center">
+    <td> * </td>
+    <td> Binary Multiplication Operation </td>
+    <td> 100 * 0.75 </td>
+    <td> 75 </td>
+  </tr>
+  <tr align="center">
+    <td> / </td>
+    <td> Binary Division Operation </td>
+    <td> 1024 / 256 </td>
+    <td> 4 </td>
+  </tr>
+  <tr align="center">
+    <td> ^ </td>
+    <td> Exponentiation </td>
+    <td> 5 ^ 5 </td>
+    <td> 3125 </td>
+  </tr>
+  <tr align="center">
+    <td> ! </td>
+    <td> Factorial </td>
+    <td> 5! </td>
+    <td> 120 </td>
+  </tr>
+  <tr align="center">
+    <td> log </td>
+    <td> Logarithm With Any Base </td>
+    <td> ln(2.718281) <br/> log6(36) </td>
+    <td> 1 <br/> 2 </td>
+  </tr>
+  <tr align="center">
+    <td> root </td>
+    <td> Any Root Extent </td>
+    <td> sqrt(100) <br/> root10(1024) </td>
+    <td> 10 <br/> 2 </td>
+  </tr>
+  <tr align="center">
+    <td> sin </td>
+    <td> Sine Function </td>
+    <td> sin(120) </td>
+    <td> 0.580611 </td>
+  </tr>
+  <tr align="center">
+    <td> cos </td>
+    <td> Cosine Function </td>
+    <td> cos(120) </td>
+    <td> 0.814181 </td>
+  </tr>
+  <tr align="center">
+    <td> tg </td>
+    <td> Tangent Function </td>
+    <td> tg(120) </td>
+    <td> 0.713123 </td>
+  </tr>
+  <tr align="center">
+    <td> ctg </td>
+    <td> Cotangent Function </td>
+    <td> ctg(120) </td>
+    <td> 1.40228 </td>
+  </tr>
+  <tr align="center">
+    <td> sec </td>
+    <td> Secant Function </td>
+    <td> sec(120) </td>
+    <td> 1.22823 </td>
+  </tr>
+  <tr align="center">
+    <td> csc </td>
+    <td> Cosecant Function </td>
+    <td> csc(120) </td>
+    <td> 1.72232 </td>
+  </tr>
+</table>
+
+### Example
+Here is examples how to use it
+```C++
+const std::string expressions[]{ "(sin(120) - cos(360)) * tg(90)",
+    "(-4) * 5.5 + 3.2 * sec(90)^2",
+    "(((((((10))))))) - (((((((5)))))))" };
+try
+{
+    calculator::Calculator calculator;
+    for (const auto& expression : expressions)
+    {
+        std::cout << calculator.eval(expression) << std::endl;
+    }
+}
+catch (std::exception& e)
+{
+    std::cout << e.what() << std::endl;
+}
+```
+
+## License
+
+Distributed under the MIT License. See [LICENSE](https://github.com/brano-san/Calculator/blob/master/LICENSE.md) for more information.
+
+## Authors
+
+* **Andrey** - *Student* - [brano-san](https://github.com/brano-san) - *All work*
+
+[![Contributors](https://img.shields.io/github/contributors/brano-san/Calculator?color=dark-green)](https://github.com/brano-san/Calculator/graphs/contributors)
+[![License](https://img.shields.io/github/license/brano-san/Calculator)](https://github.com/brano-san/Calculator/blob/master/LICENSE.md) 
